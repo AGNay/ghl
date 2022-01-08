a8ee67f1709b2d735ed187494c21ed4f33c050b0
AGNay <35140130+AGNay@users.noreply.github.com>
2022-01-08 22:35:26 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -7,7 +7,7 @@ Firmware_Diy_Core() {
 	Author=AUTO
 	Author_URL=AUTO
 	Default_IP="192.168.1.1"
-	Banner_Message="Powered by AutoBuild-Actions"
+	Banner_Message="Powered by Zau"
 
 	Short_Firmware_Date=true
 	Checkout_Virtual_Images=false
